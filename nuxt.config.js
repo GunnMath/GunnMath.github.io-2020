85707e55111beee216a1e9005967eea39491ec34
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-07-27 19:07:16 -0400

    page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'gunnmathcircle',
+    title: 'Gunn Math Circle',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
