e8e78e3746d7b17a39fd8234f7fb3da2fa693b68
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-07-27 18:47:19 -0400

    working menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,45 @@
 <template>
-  <div>
+  <div class='w-full bg-gunn-50'>
+    <nav class='w-full flex-col md:flex-row flex md:px-4 bg-gunn-500'>
+      <a class='flex flex-row mx-auto md:mx-0 mb-2 md:mb-0 md:mr-5 py-3'>
+        <img class='h-10 w-10 mr-3' src='https://cdn.discordapp.com/icons/736075025299537980/53f874b8a1e7eb4675ec819f49a89446.webp?size=256'/>
+        <span class='block my-auto text-white font-bold tracking-wide text-3xl mr-5 md:mr-0'>Gunn Math Circle</span>
+      </a>
+      <span @click='showNav=!showNav' class='flex top-4 right-5 cursor-pointer h-10 w-10 rounded-md hover:bg-gunn-600 absolute md:hidden justify-center items-center text-white text-2xl'>
+        &times;
+      </span>
+      <div :class='[!showNav? "hidden md:flex":"flex"]' class='flex-col  md:flex-row md:flex-grow'>
+        <a class='block md:h-full md:inline-flex justify-center items-center  md:px-5 text-center py-2 md:py-0 text-white  hover:underline hover:bg-gunn-600' :href='href' v-for='{name, href} in navLinks'>
+          {{name}}
+        </a>
+      </div>
+
+    </nav>
     <Nuxt />
   </div>
 </template>
-
+<script>
+export default {
+  data:()=>({
+    showNav:false,
+    navLinks:[
+      {
+        name:'Officers',
+        href:'/officers'
+      },
+      {
+        name:'Contests',
+        href:'/contests'
+      },
+      {
+        name:'About',
+        href:'/about'
+      }
+    ]
+  })
+}
+</script>
 <style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,30 @@ module.exports = {
     './layouts/**/*.vue',
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
+    './nuxt.config.{js,ts}'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gunn': {
+          DEFAULT: '#BD1A18',
+          '50': '#F7C4C3',
+          '100': '#F5ADAC',
+          '200': '#EF817F',
+          '300': '#E95452',
+          '400': '#E32725',
+          '500': '#BD1A18',
+          '600': '#A21615',
+          '700': '#871311',
+          '800': '#6C0F0E',
+          '900': '#500B0A'
+        }
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
